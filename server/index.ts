f3de5c163ee27d55e73fca71e2a72dc855dd815c
suman sinha <sumansinha437@gmail.com>
2025-07-29 23:55:57 +0530

    updated the cors configuration

--- server/index.ts
+++ server/index.ts
@@ -9,7 +9,11 @@ dotenv.config();
 connectDB();
 
 const app=express();
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.use('/api/auth',authRouter);
